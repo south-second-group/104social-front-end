b9f2e902e1310edaaa83b1f40e5d922fcc4b1924
snowman12320 <snowman12320@gmail.com>
2024-05-13 20:55:32 +0800

    feat: 手刻下拉選單元件

---
+++ components/DropdownComp.vue
@@ -0,0 +1,76 @@
+<script setup>
+const props = defineProps({
+  options: Array,
+  size: String,
+  disabled: Boolean,
+})
+const selectedOption = defineModel()
+
+const isOptionsExpanded = ref(false)
+</script>
+
+<template>
+  <div
+    class="relative cursor-pointer "
+    :class="{
+      'w-[514px]': props.size === 'lg',
+      'w-[303px]': props.size === 'sm',
+      'pointer-events-none opacity-90': props.disabled,
+    }"
+  >
+    <button
+      class="flex w-full items-center justify-between rounded-lg border bg-white px-[10px] py-[6px] text-[14px] text-[#A1A1AA] hover:border-primary-dark"
+      :class="{ 'border-primary-dark': isOptionsExpanded }"
+      @click="isOptionsExpanded = !isOptionsExpanded"
+      @blur="isOptionsExpanded = false"
+    >
+      <span>{{ selectedOption }}</span>
+      <svg
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        class="size-4 transition-transform duration-200 ease-in-out"
+        :class="isOptionsExpanded ? 'rotate-180' : 'rotate-0'"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </button>
+
+    <transition
+      enter-active-class="transform transition duration-500 ease-custom"
+      enter-class="-translate-y-1/2 scale-y-0 opacity-0"
+      enter-to-class="translate-y-0 scale-y-100 opacity-100"
+      leave-active-class="transform transition duration-300 ease-custom"
+      leave-class="translate-y-0 scale-y-100 opacity-100"
+      leave-to-class="-translate-y-1/2 scale-y-0 opacity-0"
+    >
+      <ul
+        v-show="isOptionsExpanded"
+        class="absolute inset-x-0 mb-4 overflow-hidden rounded-lg bg-white shadow-lg"
+      >
+        <li
+          v-for="(option, index) in props.options"
+          :key="index"
+          class="m-1 rounded-lg px-3 py-2 text-start text-[16px] transition-colors duration-300 hover:bg-[#F5F5F5]"
+          @mousedown.prevent="
+            isOptionsExpanded = false;
+            selectedOption = option;
+          "
+        >
+          {{ option }}
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<style scoped>
+.ease-custom {
+  transition-timing-function: cubic-bezier(0.61, -0.53, 0.43, 1.43);
+}
+</style>
--- components/FigmaDemo.vue
+++ components/FigmaDemo.vue
@@ -2,32 +2,25 @@
 const open = ref(false)
 const temp = ref(false)
 const selectedItem = ref(null) // v-model 我測試無法用，只好用 click 事件 ，應該要可以，等勇者來解
-const searchValue = ref('')
-
 const items = [
   [
     {
       label: 'Profile',
-      click: () => {
-        selectedItem.value = 'Profile'
-      },
     },
     {
       label: 'Profile2',
-      click: () => {
-        selectedItem.value = 'Profile2'
-      },
+      click: () => (selectedItem.value = 'Profile2'),
     },
   ],
 ]
 
-defineShortcuts({
-  o: () => (open.value = !open.value),
-})
+const isCheck = ref(false)
+const searchValue = ref('')
+
+const selectedOption = ref('請選擇性別')
+const options = ['男', '女', '不透露']
 
 const isChinese = char => /[\u4E00-\u9FA5]/.test(char)
-
-const isCheck = ref(false)
 </script>
 
 <template>
@@ -112,53 +105,76 @@ const isCheck = ref(false)
       class="search-sm"
     />
 
-    <UDropdown
-      v-model:open="temp"
-      :items="items"
-      :popper="{ placement: 'bottom-start' }"
-      :ui="{
-        width: 'w-[303px]',
-        ring: 'ring-1 ring-gray-100',
-        shadow: 'shadow-md',
-      }"
-      class="dropdown-sm pointer-events-none opacity-60"
-      :class="{
-        border: temp,
-      }"
-      disabled
-    >
-      <UButton
-        color="white"
-        label="請選擇性別"
-        trailing-icon="i-heroicons-chevron-down-20-solid"
-        class="text-[#A1A1AA]"
-      />
-    </UDropdown>
+    <!-- 棄用無法取值 -->
+    <section v-show="false">
+      <UDropdown
+        v-model:open="temp"
+        :items="items"
+        :popper="{ placement: 'bottom-start' }"
+        :ui="{
+          width: 'w-[303px]',
+          ring: 'ring-1 ring-gray-100',
+          shadow: 'shadow-md',
+        }"
+        class="dropdown-sm pointer-events-none opacity-60"
+        :class="{
+          border: temp,
+        }"
+        disabled
+      >
+        <UButton
+          color="white"
+          label="請選擇性別"
+          trailing-icon="i-heroicons-chevron-down-20-solid"
+          class="text-[#A1A1AA]"
+        />
+      </UDropdown>
+      <UDropdown
+        v-model:open="open"
+        :items="items"
+        :popper="{ placement: 'bottom-start' }"
+        :ui="{
+          width: 'w-[514px]',
+          ring: 'ring-1 ring-gray-100',
+          shadow: 'shadow-md',
+        }"
+        class="dropdown-lg"
+        :class="{
+          border: open,
+        }"
+      >
+        <UButton
+          color="white"
+          :label="selectedItem || '請選擇性別'"
+          trailing-icon="i-heroicons-chevron-down-20-solid"
+          class="text-[#A1A1AA]"
+        />
+        <template #item="{ item }">
+          <li
+            :key="item"
+            class="dropdown-item list-none"
+            @click="selectedItem = item.label"
+          >
+            <p>{{ item.label }}</p>
+          </li>
+        </template>
+      </UDropdown>
+      {{ selectedItem || '請選擇性別' }}
+    </section>
 
-    <UDropdown
-      v-model="selectedItem"
-      v-model:open="open"
-      :items="items"
-      :popper="{ placement: 'bottom-start' }"
-      :ui="{
-        width: 'w-[514px]',
-        ring: 'ring-1 ring-gray-100',
-        shadow: 'shadow-md',
-      }"
-      class="dropdown-lg"
-      :class="{
-        border: open,
-      }"
-      @change="console.log('change')"
-    >
-      <UButton
-        color="white"
-        :label="selectedItem || '請選擇性別'"
-        trailing-icon="i-heroicons-chevron-down-20-solid"
-        class="text-[#A1A1AA]"
-      />
-    </UDropdown>
-    {{ selectedItem || '請選擇性別' }}
+    <DropdownComp
+      v-model="temp"
+      :options="options"
+      size="sm"
+      :disabled="true"
+    />
+    <DropdownComp
+      v-model="selectedOption"
+      :options="options"
+      size="lg"
+      :disabled="false"
+    />
+    {{ selectedOption || '請選擇性別' }}
 
     <div>
       <h2 class="text-H3">
@@ -219,7 +235,7 @@ const isCheck = ref(false)
 
     <div class="flex gap-10">
       <CollectionBtn v-model="isCheck" />
-      <p>defineModel() : {{ isCheck }}</p>
+      <p>defineModel() isCheck : {{ isCheck }}</p>
       <!-- https://muki.tw/vmodel-definemodel-props-emit/?fbclid=IwAR3BMyVyKW6AwjtzYeP7rH6Nvnv6xRr615D9X1L2Y6yxVRg-S8xv5kfhO8g -->
       <!-- https://github.com/vuejs/rfcs/discussions/503 -->
     </div>
