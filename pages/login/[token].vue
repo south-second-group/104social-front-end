1a4816cac0bd7366c0ae1976984f1faf2775e8c2
kungyu-hui <evaevakung33@gmail.com>
2024-06-01 17:41:37 +0800

    fix:修正google登入toast顯示錯誤的問題

--- pages/login/[token].vue
+++ pages/login/[token].vue
@@ -76,7 +76,7 @@ useAsyncData(async () => {
   if (token.value) {
     try {
       const res = await auth.googleWriteCookie(token.value)
-      if (res.status || res.status === 302) {
+      if (res.status) {
         localStorage.setItem('avatar', res.data.avatar)
         toast('登入成功！正在跳轉...', 'success')
         setTimeout(() => {
@@ -85,7 +85,12 @@ useAsyncData(async () => {
       }
     }
     catch (error) {
-      toast('Google 登入失敗，請重試', 'error')
+      const status = error.response.status
+      if (status === 400) {
+        toast('Google 登入失敗，請重試', 'error')
+      } else {
+        toast('登入成功！正在跳轉...', 'success')
+      }
     }
   }
 })
