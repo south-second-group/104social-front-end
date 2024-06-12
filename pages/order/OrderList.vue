337feef83a21e6b798dc7091a93974d9c69db4de
kungyu-hui <evaevakung33@gmail.com>
2024-06-12 08:53:47 +0800

    feat:新增儲值列表頁

---
+++ pages/order/OrderList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container py-5 lg:py-20">
+    <h3 class="text-H4 md:text-H3 mb-2">
+      104 緣來福袋
+    </h3>
+    <h4 class="text-H5 md:text-H4 mb-6 text-neutral-400 md:mb-10">
+      提升您的相親體驗，解鎖更多精彩功能！
+    </h4>
+    <div class="flex flex-col justify-center gap-6 text-start md:flex-row">
+      <!-- 訂閱方案 -->
+      <OrderSubscriptionPlan />
+      <!-- 儲值方案 -->
+      <OrderRechargePlan />
+    </div>
+  </div>
+</template>
