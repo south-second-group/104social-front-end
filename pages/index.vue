e39cadc1f0ad256f35d529ecf3f0580e1f452f33
Otis Chen <variety928@gmail.com>
2024-06-16 22:02:14 +0800

    調整 meta 資料

--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,22 @@ import { appDescription, appKeywords, appName } from '@/constants'
 
 useHead({
   meta: [
+    { name: 'google-site-verification', content: 'zs_wqfJ13a7s9kJIrbJRRq5NGh1k4XRgoIFSbIou360' },
     { name: 'title', content: appName },
     { name: 'description', content: appDescription },
     { name: 'keywords', content: appKeywords },
     { property: 'og:title', content: appName },
     { property: 'og:description', content: appDescription },
-    { property: 'og:image', content: '../assets/img/logo.png' },
+    { property: 'og:image', content: '~/assets/img/logo.png' },
   ],
 })
 </script>
 
 <template>
-  <ClientOnly>
-    <indexBanner />
-    <indexSelectedMembers />
-    <indexArticles />
-    <indexStory />
-    <indexSubmitStory />
-    <indexCallToAction />
-  </ClientOnly>
+  <indexBanner />
+  <indexSelectedMembers />
+  <indexArticles />
+  <indexStory />
+  <indexSubmitStory />
+  <indexCallToAction />
 </template>
