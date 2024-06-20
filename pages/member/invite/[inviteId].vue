58725c8ec21250b6118d81a34a64887024f20f6a
kungyu-hui <evaevakung33@gmail.com>
2024-06-20 13:55:09 +0800

    feat:新增邀約列表頁

--- pages/member/invite/[inviteId].vue
+++ pages/member/invite/[inviteId].vue
@@ -1,12 +1,55 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { inviteListApi } from '~/apis/repositories/inviteList'
+
+const route = useRoute()
+const router = useRouter()
+const inviteId = route.params.inviteId
+const type = route.query.type // 获取查询参数
+const invitationDetails = ref(null)
+
+// 取得 API 會員詳細資料
+async function fetchInviteDetail() {
+  try {
+    let response
+    if (type === 'who') {
+      response = await inviteListApi.getInviteWho(inviteId)
+      invitationDetails.value = response.data
+    }
+    else if (type === 'me') {
+      response = await inviteListApi.getInviteMe(inviteId)
+      invitationDetails.value = response.data
+    }
+  }
+  catch (error) {
+    console.error('會員詳細資料錯誤訊息:', error)
+  }
+}
+
+onMounted(() => {
+  fetchInviteDetail()
+})
+
+function goBack() {
+  router.push('/member/invite')
+}
 </script>
 
 <template>
-  <div class=" container border p-3 text-start md:p-12">
+  <div class="container border p-3 text-start md:p-12">
     <div class="mx-auto max-w-[700px]">
-      <utilsPhotoCaroucel />
+      <div class="shrink-0">
+        <img
+          :src="type === 'who'
+            ? invitationDetails?.profileByInvitedUser?.[0]?.photoDetails?.photo
+            : invitationDetails?.profileByUser?.[0]?.photoDetails?.photo"
+          alt="Profile Photo"
+          class="mx-auto size-[150px] rounded-full object-cover object-top"
+        >
+      </div>
       <h1 class="text-H4 mt-24">
-        {{ true ? '邀請人資訊' : '被邀請人資訊' }}
+        {{ type === 'who' ? '邀請人資訊' : '被邀請人資訊' }}
       </h1>
 
       <div class="mt-6">
@@ -15,61 +58,72 @@
             <label for="">姓名：</label>
             <ClientOnly>
               <span
-                class=""
                 :class="{
                   'font-montserrat': !useIsChineseFunc(
-                    'aaa',
+                    type === 'who'
+                      ? invitationDetails?.profileByInvitedUser?.[0]?.nickNameDetails?.nickName
+                      : invitationDetails?.profileByUser?.[0]?.nickNameDetails?.nickName,
                   ),
                 }"
-              >蔡佩芬</span>
+              >
+                {{
+                  type === 'who'
+                    ? invitationDetails?.profileByInvitedUser?.[0]?.nickNameDetails?.nickName
+                    : invitationDetails?.profileByUser?.[0]?.nickNameDetails?.nickName
+                }}
+              </span>
             </ClientOnly>
           </div>
           <div>
             <label for="">年收入：</label>
-            <span class="">60萬-90萬</span>
+            <span class="">{{
+              type === 'who'
+                ? invitationDetails?.profileByInvitedUser?.[0]?.incomeDetails?.income
+                : invitationDetails?.profileByUser?.[0]?.incomeDetails?.income
+            }}</span>
           </div>
           <div>
             <label for="">工作：</label>
-            <span class="">營養師</span>
+            <span class="">{{
+              type === 'who'
+                ? invitationDetails?.profileByInvitedUser?.[0]?.jobDetails?.job
+                : invitationDetails?.profileByUser?.[0]?.jobDetails?.job
+            }}</span>
           </div>
           <div class="font-montserrat">
             <label for="">Line ID：</label>
-            <span>bb0909123</span>
+            <span>{{
+              type === 'who'
+                ? invitationDetails?.profileByInvitedUser?.[0]?.lineDetails?.id
+                : invitationDetails?.profileByUser?.[0]?.lineDetails?.id
+            }}</span>
           </div>
 
-          <div class="col-span-2  font-montserrat">
-            <label for="">蔡佩芬的標籤：</label>
-            <div class="block  space-x-2 space-y-2 rounded-md px-3 py-1">
+          <div class="col-span-2 font-montserrat">
+            <label for="">
+              {{
+                type === 'who'
+                  ? invitationDetails?.profileByInvitedUser?.[0]?.nickNameDetails?.nickName
+                  : invitationDetails?.profileByUser?.[0]?.nickNameDetails?.nickName
+              }}的標籤：
+            </label>
+            <div class="block space-x-2 space-y-2 rounded-md px-3 py-1">
               <UBadge
-                v-for="i in 10"
-                :key="i"
-                class="btn-withIcon-outline-rwd  pointer-events-none  !rounded-lg  !px-1 !py-[2px] "
+                v-for="(tag, index) in (
+                  type === 'who'
+                    ? invitationDetails?.profileByInvitedUser?.[0]?.tags
+                    : invitationDetails?.profileByUser?.[0]?.tags
+                )"
+                :key="index"
+                class="btn-withIcon-outline-rwd pointer-events-none !rounded-lg !px-1 !py-[2px]"
               >
                 <p
                   class="!text-[10px]"
                   :class="{
-                    'font-montserrat': !useIsChineseFunc(
-                      'Badge',
-                    ),
+                    'font-montserrat': !useIsChineseFunc(tag),
                   }"
                 >
-                  Badge
-                </p>
-              </UBadge>
-              <UBadge
-                v-for="i in 10"
-                :key="i"
-                class="btn-withIcon-outline-rwd pointer-events-none !rounded-lg  !px-1 !py-[2px] "
-              >
-                <p
-                  class="!text-[10px]"
-                  :class="{
-                    'font-montserrat': !useIsChineseFunc(
-                      '中文字',
-                    ),
-                  }"
-                >
-                  中文字
+                  {{ tag }}
                 </p>
               </UBadge>
             </div>
@@ -78,35 +132,38 @@
 
         <div class="mt-12 w-full space-y-3">
           <label
-            class=" text-H4"
+            class="text-H4"
             for=""
-          > {{ true ? '邀請人資訊' : '被邀請人資訊' }}
+          > {{ type === 'who' ? '邀請人資訊' : '被邀請人資訊' }}
           </label>
-          <p class=" rounded-md border-2 p-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A commodi
-            molestiae tenetur
-            ipsa
-            temporibus quis distinctio tempore, minus amet voluptates deserunt tempora culpa veniam eveniet facere ea
-            optio sapiente libero!
+          <p class="rounded-md border-2 p-3">
+            {{ invitationDetails?.message.content }}
           </p>
         </div>
 
         <div class="mt-12 w-full space-y-3">
           <label
-            class=" text-H4"
+            class="text-H4"
             for=""
           >整體評價</label>
-          <div class="flex  ">
+          <div class="flex">
             <icon-heroicons:heart-solid
-              v-for=" i in 4"
+              v-for="i in (
+                type === 'who'
+                  ? invitationDetails?.profileByInvitedUser?.[0]?.userStatus?.commentCount
+                  : invitationDetails?.profileByUser?.[0]?.userStatus?.commentCount
+              )"
               :key="i"
-              class="size-10  text-primary-dark"
+              class="size-10 text-primary-dark"
             />
           </div>
         </div>
 
         <section class="mt-12 flex justify-center">
-          <button class="  px-[20px] py-[8px] text-[16px] leading-[24px] text-primary-dark ">
+          <button
+            class="px-[20px] py-[8px] text-[16px] leading-[24px] text-primary-dark"
+            @click="goBack"
+          >
             <p>返回我的邀約</p>
           </button>
         </section>
--- pages/member/invite/index.vue
+++ pages/member/invite/index.vue
@@ -1,29 +1,83 @@
 <script setup>
+import { inviteListApi } from '~/apis/repositories/inviteList'
+
+// 處理 tab 切換
 useHead({
   title: '我的邀約',
 })
 
 const tabItems = [
-  {
-    label: '我邀約誰',
-  },
-  {
-    label: '誰邀約我',
-  },
+  { label: '我邀約誰' },
+  { label: '誰邀約我' },
 ]
+
 const selectedItem = ref('我邀約誰')
 
 function onChange(index) {
   selectedItem.value = tabItems[index].label
+  // page.value = 1
+  loadData()
 }
 
+// 分頁
 const page = ref(1)
-const items = ref(Array(75))
+const itemsPerPage = 6
+const totalItems = ref(0)
+
+// 狀態
+const isDataLoading = ref(true)
+
+// 我的邀約列表
+const inviteWhoListData = ref([])
+async function getInviteWhoList() {
+  try {
+    const res = await inviteListApi.getInviteWhoList()
+    inviteWhoListData.value = res.data.invitations
+    totalItems.value = inviteWhoListData.value.length
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+// 誰邀約我列表
+const inviteMeListData = ref([])
+async function getInviteMeList() {
+  try {
+    const res = await inviteListApi.getInviteMeList()
+    inviteMeListData.value = res.data.beInvitations
+    totalItems.value = inviteMeListData.value.length
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+async function loadData() {
+  isDataLoading.value = true
+  if (selectedItem.value === '誰邀約我')
+    await getInviteMeList()
+  else
+    await getInviteWhoList()
+
+  isDataLoading.value = false
+}
+
+// 頁面載入時顯示我的邀約列表
+onMounted(loadData)
+
+// 監聽 tab 變化，根據選擇顯示相應的 API
+watch(selectedItem, loadData)
+
+// 監聽子組件的 refreshList 事件
+function handleRefreshList() {
+  loadData()
+}
 </script>
 
 <template>
-  <main class="useIsChineseFunc max-w-custom-container">
-    <div class="m-auto max-w-[856px] px-3 md:px-0">
+  <div class="container">
+    <div class="mx-auto max-w-[856px] px-3 md:px-0">
       <h1 class="text-H4 md:text-H3 mb-6 mt-[80px] text-start md:mb-[40px]">
         我的邀約
       </h1>
@@ -40,8 +94,7 @@ const items = ref(Array(75))
             },
             tab: {
               base: 'text-B2 text-neutral-400 border-b-2 border-white box-border px-4 py-2 hover:text-neutral-700 font-normal',
-              active:
-                '!text-primary-dark border-b-2 !border-primary-dark font-[700]',
+              active: '!text-primary-dark border-b-2 !border-primary-dark font-[700]',
               rounded: 'rounded-none',
               height: 'h-10',
             },
@@ -51,38 +104,63 @@ const items = ref(Array(75))
         @change="onChange"
       />
 
-      <!-- 卡片 -->
+      <!-- 卡片區域 -->
       <section
         v-if="selectedItem === '我邀約誰'"
         class="space-y-3"
       >
-        <utilsUserCardBgLight
-          v-for="i in 6"
-          :key="i"
-          :i="i"
-        />
+        <div
+          v-if="isDataLoading"
+          class="space-y-3"
+        >
+          <utilsUserCardSkeleton
+            v-for="i in itemsPerPage"
+            :key="i"
+          />
+        </div>
+        <div
+          v-else
+          class="space-y-3"
+        >
+          <MemberInviteUserCardListWho
+            v-for="(value, key) in inviteWhoListData"
+            :key="key"
+            :result-item="value"
+            :is-trash-icon="false"
+            @refresh-who-list="handleRefreshList"
+          />
+        </div>
       </section>
 
+      <!-- 誰邀約我 -->
       <section
         v-else
         class="space-y-3"
       >
-        <utilsInviteCard
-          v-for="i in 6"
-          :key="i"
-          :i="i"
-        />
-      </section>
-
-      <!-- 分頁 -->
-      <section class="py-12 font-montserrat">
-        <utilsPaginationComp
-          v-model="page"
-          :items="items"
-        />
+        <div
+          v-if="isDataLoading"
+          class="space-y-3"
+        >
+          <utilsUserCardSkeleton
+            v-for="i in itemsPerPage"
+            :key="i"
+          />
+        </div>
+        <div
+          v-else
+          class="space-y-3"
+        >
+          <MemberInviteUserCardListMe
+            v-for="(value, key) in inviteMeListData"
+            :key="key"
+            :result-item="value"
+            :is-trash-icon="false"
+            @refresh-me-list="handleRefreshList"
+          />
+        </div>
       </section>
     </div>
-  </main>
+  </div>
 </template>
 
 <style scoped></style>
