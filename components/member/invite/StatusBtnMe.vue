d91f41cb8c5dc0ab32a66703bc318312136e6b6f
kungyu-hui <evaevakung33@gmail.com>
2024-06-20 15:56:20 +0800

    feat:新增被邀約方狀態

---
+++ components/member/invite/StatusBtnMe.vue
@@ -0,0 +1,49 @@
+<script setup>
+const props = defineProps({
+  status: {
+    type: String,
+    required: true,
+  },
+})
+
+function getBtnClass(status) {
+  switch (status) {
+    case 'pending':
+    case 'finishDating':
+      return 'bg-special-info'
+    case 'cancel':
+      return 'bg-neutral-400'
+    case 'accept':
+      return 'bg-special-success'
+    case 'rejected':
+      return 'bg-special-danger'
+    default:
+      return 'bg-social-gradient-default'
+  }
+}
+
+function getBtnText(status) {
+  switch (status) {
+    case 'pending':
+    case 'finishDating':
+      return '已完成約會'
+    case 'cancel':
+      return '對方取消邀約'
+    case 'accepted':
+      return '已接受邀約'
+    case 'rejected':
+      return '已拒絕邀約'
+    default:
+      return '尚未邀約'
+  }
+}
+</script>
+
+<template>
+  <button
+    :class="getBtnClass(props.status)"
+    class="text-B3 pointer-events-none rounded-full px-3 py-1 text-white "
+  >
+    {{ getBtnText(props.status) }}
+  </button>
+</template>
