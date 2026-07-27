bc66a13edeb9ab40da71585d74d342cbfb86afb1
william <101874444+snowman12320@users.noreply.github.com>
2026-07-27 16:26:45 +0800

    Update background image for William tab

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -57,7 +57,7 @@ function hoverHandler(tab) {
         data-order="里程碑七 William"
         :class="{ 'item-hover': hoveredTab === 'william' }"
         style="
-          background-image: url('https://firebasestorage.googleapis.com/v0/b/social-e030c.appspot.com/o/about%2FIMG_5026.jpg?alt=media&token=126ed273-0959-44d8-879e-8647c06d335c');"
+          background-image: url('https://avatars.githubusercontent.com/u/101874444?v=4');"
         @mouseenter="hoverHandler('william')"
       ></div>
     </div>
